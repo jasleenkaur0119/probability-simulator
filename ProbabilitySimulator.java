ec38124aa4e38e427a8f058c240903307b8db881
jasleenkaur0119 <jasleenkaur0119@gmail.com>
2026-03-19 00:53:12 -0400

    Update ProbabilitySimulator.java

--- ProbabilitySimulator.java
+++ ProbabilitySimulator.java
@@ -1,5 +1,5 @@
 // This program simulates rolling a fair six-sided die multiple times
-//to analyze probability distribution and expected value using a Monte Carlo approach.
+// to analyze probability distribution and expected value using a Monte Carlo approach.
 
 import java.util.Scanner;
 public class ProbabilitySimulator{
@@ -7,40 +7,41 @@ public class ProbabilitySimulator{
     {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Probability Simulator");
-        //User input for number of trials is stored 
+        // User input for number of trials is stored 
         System.out.println("Enter number of trials: ");
         int trials = keyboard.nextInt();
 
         int sum = 0;
-        //Frequency of each outcome (1-6) is stored
+        // Frequency of each outcome (1-6) is stored
         int[] counts = new int[6];
 
-        //Runs a simulation of dice rolls
+        // Runs a simulation of dice rolls
         for(int i = 0; i < trials; i++)
         {
-            //Generates random number from 1 to 6
+            // Generates random number from 1 to 6
             int roll = (int)(Math.random() *6) + 1;
             sum+=roll;
-            //maps values from 1 to 6 to indices 0 to 5
+            // Maps values from 1 to 6 to indices 0 to 5
             counts[roll-1]++;
         }
-        //calculates experimental average from simulation
+        // Calculates experimental average from simulation
         double experimentalAvg = (double) sum/trials;
-        // theoretical expected value of a fair die:
+        // Theoretical expected value of a fair die:
         // E[X] = (1 + 2 + 3 + 4 + 5 + 6) / 6 = 21/6 = 3.5
-        // each outcome has equal probability (1/6), so this is the Long-run average 
+        // Each outcome has equal probability (1/6), so this is the Long-run average 
         double expectedValue = 3.5;
-        
+        // As the number of trials increases, experimentalAvg should approach expectedValue
+        // due to the law of large numbers
         System.out.println("Results: ");
         System.out.println("Trials: " + trials);
         System.out.println("Experimental Average: " + experimentalAvg);
         System.out.println("Theoretical Expected Value: " + expectedValue);
         
-        // difference shows how far simulation is from theoretical value
+        // Difference shows how far simulation is from theoretical value
         double difference = Math.abs(experimentalAvg - expectedValue);
         System.out.println("Difference: " + difference);
 
-        // displays frequency and probability distribution
+        // Displays frequency and probability distribution
         System.out.println("Distribution: ");
         for (int i = 0; i<6; i++)
         {
